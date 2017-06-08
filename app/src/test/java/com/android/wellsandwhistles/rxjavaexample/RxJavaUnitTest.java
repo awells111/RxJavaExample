d87a463bc802c67628883aa64d925e537fe932cd
awells111 <awells111@gmail.com>
2017-06-07 22:14:41 -0400

    Initial Commit
    From http://www.vogella.com/tutorials/RxJava/article.html

---
+++ app/src/test/java/com/android/wellsandwhistles/rxjavaexample/RxJavaUnitTest.java
@@ -0,0 +1,20 @@
+package com.android.wellsandwhistles.rxjavaexample;
+
+import org.junit.Test;
+
+import io.reactivex.Observable;
+
+import static org.junit.Assert.assertTrue;
+
+public class RxJavaUnitTest {
+    String result = "";
+
+    // Simple subscription to a fix value
+    @Test
+    public void returnAValue() {
+        result = "";
+        Observable<String> observer = Observable.just("Hello"); // provides datea
+        observer.subscribe(s -> result = s); // Callable as subscriber
+        assertTrue(result.equals("Hello"));
+    }
+}
