0d51fc18382333350da2e8b8baf06fc3a6b3299b
awells111 <awells111@gmail.com>
2017-06-07 22:32:17 -0400

    Tutorial "Completed"

--- app/src/main/java/com/android/wellsandwhistles/rxjavaexample/MainActivity.java
+++ app/src/main/java/com/android/wellsandwhistles/rxjavaexample/MainActivity.java
@@ -1,13 +1,32 @@
 package com.android.wellsandwhistles.rxjavaexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent i = null;
+        switch (view.getId()) {
+            case R.id.first:
+                i = new Intent(this, RxJavaSimpleActivity.class);
+                break;
+            case R.id.second:
+                i = new Intent(this, ColorsActivity.class);
+                break;
+            case R.id.third:
+                i = new Intent(this, BooksActivity.class);
+                break;
+        }
+        startActivity(i);
     }
 }
